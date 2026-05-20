a4bf6e6634be0b3b173d0637c29c62f6e26cb07f
Thiago Moura <76569184+ThiagoMoura963@users.noreply.github.com>
2026-05-20 20:23:28 +0000

    feat: add `database.js` and `.env` file

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,9 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log("Resultado:", result.rows);
+  response.status(200).json({ chave: "valor" });
+}
+
+export default status;
